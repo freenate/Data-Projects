41881118bd212af00af504d5f357d25512db21aa
freenate <38407162+freenate@users.noreply.github.com>
2019-07-16 14:59:20 -0700

    Add files via upload

---
+++ sample SQL queries/PercentIncreaseListPriceAW.sql
@@ -0,0 +1,8 @@
+--This query shows the percent increase of list prices using the start dates from the Adventure Works 2017 database and the tables Production.Product and Production.PriceListHistory
+
+select Name as "Product Name", id as "Product ID", case when PreviousPrice = 0 then 0
+else (CurrentPrice - PreviousPrice)/PreviousPrice * 100. end as "% Increase", CurrentPrice as "List Price", SDate as "Date"
+from (select ProductID as id, ListPrice as CurrentPrice, 
+      LAG(ListPrice, 1, 0) over (partition by ProductID order by StartDate) as PreviousPrice, StartDate as SDate 
+      from Production.ProductListPriceHistory) as PriceLag, Production.Product as prod
+where prod.ProductID = PriceLag.id
