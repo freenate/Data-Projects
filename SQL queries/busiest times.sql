d73f51a140dc7cea70a0a4e0bd0bd9c094213b80
Nate Freeman <38407162+freenate@users.noreply.github.com>
2019-11-07 20:36:08 -0800

    Add files via upload

---
+++ SQL queries/busiest times.sql
@@ -0,0 +1,13 @@
+(select
+case 
+when hour(date_time) in(7,8,9,10,11) then 'mornings'
+when hour(date_time) in (12,13,14,15,16) then 'afternoons'
+else 'evenings'
+end as time_period, 
+round(sum(total)/count(distinct date(date_time)),2) as revenue from supermarket
+group by time_period
+order by revenue desc)
+union
+(select hour(date_time) as time_period, round(sum(total)/count(distinct date(date_time)), 2) as revenue from supermarket
+group by hour(date_time)
+order by revenue desc)
