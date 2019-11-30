1bb924e8e2fe207f9f61fa4b1ae2049adc6354cf
Nate Freeman <38407162+freenate@users.noreply.github.com>
2019-11-29 20:06:39 -0800

    Update kpis by state.sql

--- SQL queries/kpis by state.sql
+++ SQL queries/kpis by state.sql
@@ -1,3 +1,4 @@
+-- for use in postgresql
 select state, region, extract(year from order_date) as "year", round(sum(profit),2) as "total profit", round(sum(sales),2) as "total sales", sum(quantity) as "total quantity", 
 count(distinct orders.order_id) as "total orders",  
 count(distinct returned.order_id) as "returns", cast(round(avg(discount*100),2) as text) || '%' as "discount per row",
