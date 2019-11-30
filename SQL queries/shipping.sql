2e40b9f9925db728aa5e5556b573106e215b876d
Nate Freeman <38407162+freenate@users.noreply.github.com>
2019-11-29 20:06:58 -0800

    Update shipping.sql

--- SQL queries/shipping.sql
+++ SQL queries/shipping.sql
@@ -1,3 +1,4 @@
+-- for use in postgresql
 select region, state, ship_mode, segment, subcategory, product_name, avg(ship_date - order_date) as "average number of days to ship" 
 from orders
 group by region, state, ship_mode, segment, subcategory, product_name
